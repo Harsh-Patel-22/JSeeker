3e3513c46deae2dfdf0c9d74c42b821aa812fae3
Harsh Patel <144122731+Harsh-Patel-22@users.noreply.github.com>
2025-12-17 09:42:25 +0530

    Converted the ProjectDetails to a list rather than a dictionary

--- Backend/Backend/DTOs/ResumeContentsDto.cs
+++ Backend/Backend/DTOs/ResumeContentsDto.cs
@@ -7,7 +7,7 @@ public class ResumeContentsDto {
     public BasicDetailsDto? BasicDetails {get; set;}
     public ContactDetailsDto? ContactDetails {get; set;}
     // TODO - Make the ProjectDetails as a list rather than a dictionary
-    public  Dictionary<string, ProjectDetailsDto> ProjectDetails {get; set;}
+    public  List<ProjectDetailsDto> ProjectDetails {get; set;}
     public List<WorkExperienceDetailsDto> WorkExperienceDetails {get; set;} 
     public List<EducationDetailsDto> EducationDetails {get; set;}
     public List<LanguageDto> LanguageDetails {get; set;}
