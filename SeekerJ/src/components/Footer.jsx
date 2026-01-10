11e7254d8ebb5bef8aae5e726d11b616d4815c84
Harsh Patel <144122731+Harsh-Patel-22@users.noreply.github.com>
2026-01-10 15:06:30 +0530

    Removed the extra tabs in the footer

--- SeekerJ/src/components/Footer.jsx
+++ SeekerJ/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 
 const Footer = () => {
-    return <footer className="py-3 bg-dark" > <ul className="nav justify-content-center border-bottom pb-3 mb-3"> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">Home</a></li> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">Features</a></li> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">Pricing</a></li> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">FAQs</a></li> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">About</a></li> </ul> <p className="text-center text-white">© JSeeker, Inc</p> </footer>
+    return <footer className="py-3 bg-dark" > <ul className="nav justify-content-center border-bottom pb-3 mb-3"> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">Home</a></li> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">Features</a></li> <li className="nav-item"><a href="#" className="nav-link px-2 text-white">About</a></li> </ul> <p className="text-center text-white">© JSeeker, Inc</p> </footer>
 }
 
 export default Footer;
