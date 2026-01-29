ee5e7df7c3a358ce578a3b50ceaebc11e92a71e1
Harsh Patel <144122731+Harsh-Patel-22@users.noreply.github.com>
2026-01-29 19:31:22 +0530

    Updated the study to upper case in the UI.

--- SeekerJ/src/components/profile/EducationSection.jsx
+++ SeekerJ/src/components/profile/EducationSection.jsx
@@ -8,7 +8,7 @@ const EducationSection = ({details}) => {
       {details && details.map((edu, index) => {
         return <div className="mb-3">
           {/* <h5 className="fw-bold mb-3">{edu.study}</h5> */}
-          <p className="mb-1 fw-semibold">{edu.study}</p>
+          <p className="mb-1 fw-semibold">{edu.study.toUpperCase()}</p>
           <p className="small mb-1">{edu.instituteName}, {edu.state}</p>
           <p className="small mb-0">{edu.startDate.substring(0, 4)} - {edu.endDate.substring(0, 4)}</p>
         </div>
