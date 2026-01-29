0199b479313092ac479a5df039c21649f8290ccf
Harsh Patel <144122731+Harsh-Patel-22@users.noreply.github.com>
2026-01-29 19:32:06 +0530

    Removed the unnecessary placeholder value

--- SeekerJ/src/components/profile/EducationSection.jsx
+++ SeekerJ/src/components/profile/EducationSection.jsx
@@ -21,15 +21,7 @@ const EducationSection = ({details}) => {
         </p>
       </div> */}
     </div>
-  )
-    {details && details.map((edu, index) => {
-      <div className="card border-0 shadow-sm rounded-4 p-4 mb-4">
-        <h5 className="fw-bold mb-3">{edu.study}</h5>
-        <p className="mb-1 fw-semibold">B.Tech in Computer Science and Engineering</p>
-        <p className="text-muted small mb-0">XYZ University, Gujarat — 2022 to 2026</p>
-      </div>
-    }    
-  )}
+  ) 
 }
 
 export default EducationSection; 
