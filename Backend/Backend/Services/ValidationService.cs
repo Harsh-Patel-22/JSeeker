c92bbc62fd6dedb8e85697bda97e2daf667963bf
Harsh Patel <144122731+Harsh-Patel-22@users.noreply.github.com>
2026-07-05 16:35:50 +0530

    Opt: Removed Redundant DB Calls and unnecessary data load.

--- Backend/Backend/Services/ValidationService.cs
+++ Backend/Backend/Services/ValidationService.cs
@@ -13,7 +13,7 @@ public class ValidationService(ApplicationDbContext context) {
         await context.Users.AnyAsync(u => u.Id == userId && u.IsHirer);
 
     public async Task<bool> IsTheirJobAsync(Guid userId, int jobId) =>
-        await context.Jobs.Where(job => job.Id == jobId).Select(job => job.HirerId).FirstOrDefaultAsync() == userId;
+        await context.Jobs.Where(job => job.Id == jobId && job.HirerId == userId).AnyAsync();
 
     public async Task<bool> JobExistsAsync(int jobId) =>
         await context.Jobs.AnyAsync(j => j.Id == jobId);
@@ -25,56 +25,51 @@ public class ValidationService(ApplicationDbContext context) {
         await context.Applications.AnyAsync(a => a.ApplicationId == applicationId);
 
     public async Task<bool> IsTheirApplicationAsync(Guid userId, int applicationId) {
-        Guid seekerId = await context.Applications.Where(application => application.ApplicationId == applicationId)
-            .Select(application => application.SeekerId).FirstOrDefaultAsync();
-        Guid hirerId = await context.Applications.Where(application => application.ApplicationId == applicationId)
-            .Select(application => application.HirerId).FirstOrDefaultAsync();
-        return seekerId == userId || hirerId == userId;
+        var ids = await context.Applications
+            .Where(appl => appl.ApplicationId == applicationId)
+            .Select(appl =>
+            new Guid[] {
+                appl.SeekerId,
+                appl.HirerId
+            })
+            .FirstOrDefaultAsync();
+        // Guid seekerId = await context.Applications.Where(application => application.ApplicationId == applicationId)
+        //     .Select(application => application.SeekerId).FirstOrDefaultAsync();
+        // Guid hirerId = await context.Applications.Where(application => application.ApplicationId == applicationId)
+        //     .Select(application => application.HirerId).FirstOrDefaultAsync();
+        return ids != null && (ids[0] == userId || ids[1] == userId);
     }
     
 
     public async Task<bool> IsTheirInterviewAsync(Guid userId, int interviewId) {
-        Guid seekerId = await context.Interviews.Where(interview => interview.ApplicationId == interviewId)
-            .Select(interview => interview.SeekerId).FirstOrDefaultAsync();
-        Guid hirerId = await context.Interviews.Where(interview => interview.ApplicationId == interviewId)
-            .Select(interview => interview.HirerId).FirstOrDefaultAsync();
-        return seekerId == userId || hirerId == userId;
+        var ids = await context.Interviews
+            .Where(i => i.ApplicationId == interviewId)
+            .Select(i =>
+            new Guid[] {
+                i.SeekerId,
+                i.HirerId
+            })
+            .FirstOrDefaultAsync();
+        // Guid seekerId = await context.Interviews.Where(interview => interview.ApplicationId == interviewId)
+        //     .Select(interview => interview.SeekerId).FirstOrDefaultAsync();
+        // Guid hirerId = await context.Interviews.Where(interview => interview.ApplicationId == interviewId)
+        //     .Select(interview => interview.HirerId).FirstOrDefaultAsync();
+        return ids != null && (ids[0] == userId || ids[1] == userId);
     }
 
     public async Task<bool> HasApplicationFor(Guid seekerId, Guid hirerId) => 
         await context.Applications.AnyAsync(appl => appl.SeekerId == seekerId && appl.HirerId == hirerId);
     public async Task<bool> HasInterviewFor(Guid seekerId, Guid hirerId) => 
         await context.Applications.AnyAsync(appl => appl.SeekerId == seekerId && appl.HirerId == hirerId);
-    
-    // public async Task<bool> GithubProjectsAlreadyStored(Guid userId) {
-    //     // GitHub Username is set to something and even some projects are added
-    //     if (!(await context.Users.Where(user => user.Id == userId).Select(user => user.GithubUsername).FirstOrDefaultAsync())!.Equals("") && (await context.Projects.Where(proj => proj.UserId == userId).ToListAsync()).Count != 0) {
-    //         return true;
-    //     }
-    //
-    //     return false;
-    // }
 
-    public async Task<bool> ApplicationAlreadyExistAsync(Guid userId, int jobId) {
-        if (await context.Applications.Where(appl => appl.SeekerId == userId && appl.JobId == jobId)
-                .FirstOrDefaultAsync() == null) {
-            return false;
-        }
-        return true;
-    }
-    public async Task<bool> InterviewAlreadyExistAsync(Guid userId, int jobId) {
-        if (await context.Interviews.Where(appl => appl.SeekerId == userId && appl.JobId == jobId)
-                .FirstOrDefaultAsync() == null) {
-            return false;
-        }
-        return true;
-    }
-    
-    public async Task<bool> CanAddMoreProjects(Guid userId) {
-        if ((await context.Projects.Where(project => project.UserId == userId).ToListAsync()).Count <= 3) {
-            return true;
-        }
 
-        return false;
-    }
+    public async Task<bool> ApplicationAlreadyExistAsync(Guid userId, int jobId) => await context.Applications
+        .Where(appl => appl.SeekerId == userId && appl.JobId == jobId).AnyAsync();
+    public async Task<bool> InterviewAlreadyExistAsync(Guid userId, int jobId) => await context.Interviews
+        .Where(appl => appl.SeekerId == userId && appl.JobId == jobId).AnyAsync();
+
+    public async Task<bool> CanAddMoreProjects(Guid userId) =>
+        await context.Projects
+            .Where(project => project.UserId == userId)
+            .CountAsync() <= 3;
 }
